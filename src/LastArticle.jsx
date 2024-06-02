f620a183315795a14162f65bbc68f5aa3282369b
Dimitri Andre <andre.d3003@gmail.com>
2024-06-02 14:01:41 +0200

    last article title

---
+++ src/LastArticle.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './LastArticle.css'; // Link to the CSS file for styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+const LastArticle = () => {
+	  return (
+		      <div className="last-article-title-box">
+			  <div class="last-article-title">
+				Derniers Articles
+		  	</div>
+		      </div>
+		    );
+};
+
+export default LastArticle
