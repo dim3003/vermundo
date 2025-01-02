806c0eb7284c1e70f8c95fa549792e87cd2ec41a
dim3003 <andre.d3003@gmail.com>
2025-01-02 17:26:43 +0100

    footer home mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './Header';
-import LastArticle from './LastArticle';
-import About from './About';
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import LastArticle from "./LastArticle";
+import About from "./About";
+import Footer from "./Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div>
-	  <Header />
-	  <LastArticle />
-	  <About />
-    </div>
-  )
+	return (
+		<div>
+			<Header />
+			<LastArticle />
+			<About />
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;
