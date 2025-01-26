66191a41327b1ceaba2c40d3cd3687cebb8f0574
dim3003 <andre.d3003@gmail.com>
2025-01-26 17:52:16 +0100

    header desktop reactive

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import './Header.css'; 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-	  return (
-		      <div className="header">
-		        <div className="top-bar">
-		          <div className="brand">VERMUNDO</div>
-		          <input type="text" className="search-bar" placeholder="Recherche"/>
-		        </div>
-		        <div className="central-text">PARTIR À L'AVENTURE</div>
-		  	<div className="central-link"><a>Vers le blog <FontAwesomeIcon icon={faArrowRight} /></a></div>
-		      </div>
-		    );
+	return (
+		<div className="header">
+			<div className="top-bar">
+				<div className="brand">VERMUNDO</div>
+			</div>
+			<div className="central-text">PARTIR À L'AVENTURE</div>
+		</div>
+	);
 };
 
-export default Header
+export default Header;
