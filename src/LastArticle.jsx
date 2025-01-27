c04d7a881210a04b41763626f3fd27f355c1f318
dim3003 <andre.d3003@gmail.com>
2025-01-27 20:20:02 +0100

    article cards desktop css

--- src/LastArticle.jsx
+++ src/LastArticle.jsx
@@ -1,35 +1,37 @@
-import React from 'react';
-import './LastArticle.css'; 
-import ArticleCard from './ArticleCard';
+import React from "react";
+import "./LastArticle.css";
+import ArticleCard from "./ArticleCard";
 
 const LastArticle = () => {
-	  return (
-		  <>
+	return (
+		<>
 			<div className="last-article-title-box">
 				<div className="last-article-title">
 					Derniers Articles
 				</div>
 			</div>
-			<ArticleCard 
-				title="Titre" 
-				text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex." 
-				date="Date de parution"
-				imageUrl="halongBay.jpeg"
-			/>
-			<ArticleCard 
-				title="Titre" 
-				text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex." 
-				date="Date de parution"
-				imageUrl="halongBay.jpeg"
-			/>
-			<ArticleCard 
-				title="Titre" 
-				text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex." 
-				date="Date de parution"
-				imageUrl="halongBay.jpeg"
-			/>
-		  </>
-	    );
+			<div className="last-article-container">
+				<ArticleCard
+					title="Titre"
+					text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex."
+					date="Date de parution"
+					imageUrl="halongBay.jpeg"
+				/>
+				<ArticleCard
+					title="Titre"
+					text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex."
+					date="Date de parution"
+					imageUrl="halongBay.jpeg"
+				/>
+				<ArticleCard
+					title="Titre"
+					text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae convallis ex."
+					date="Date de parution"
+					imageUrl="halongBay.jpeg"
+				/>
+			</div>
+		</>
+	);
 };
 
-export default LastArticle
+export default LastArticle;
