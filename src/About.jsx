236a31ff631762e09745afb338e7575d51137f48
dim3003 <andre.d3003@gmail.com>
2025-01-28 20:55:32 +0100

    about section desktop version

--- src/About.jsx
+++ src/About.jsx
@@ -3,33 +3,54 @@ import "./About.css";
 
 const About = () => {
 	return (
-		<div className="about-container">
-			<div className="about-title">À propos</div>
-			<div className="about-content">
-				<img
-					src="/images/maeJeanJacket.JPG"
-					alt="Profile"
-					className="profile-image"
-				/>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur
-					adipiscing elit. Aliquam vitae convallis
-					ex. Sed ut nulla id magna consectetur
-					tincidunt. Quisque rhoncus ipsum ut
-					iaculis iaculis. Nunc non vestibulum
-					justo, in hendrerit tellus. Vestibulum
-					ac vulputate nulla. Duis id eros orci.
-					Morbi sem sapien, varius sit amet lorem
-					tincidunt, pulvinar viverra ex. Aliquam
-					feugiat odio sem, non semper augue
-					malesuada cras.
-				</p>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur
-					adipiscing elit. Aliquam vitae convallis
-					ex. Sed ut nulla id magna consectetur
-					tincidunt.
-				</p>
+		<>
+			<div className="about-container">
+				<div className="about-title-mobile">
+					À propos
+				</div>
+				<div className="about-content">
+					<div className="about-image-block">
+						<img
+							src="/images/maeJeanJacket.JPG"
+							alt="Profile"
+							className="about-image"
+						/>
+					</div>
+					<div className="about-description">
+						<div className="about-title-desktop">
+							À Propos
+						</div>
+						<p>
+							Lorem ipsum dolor sit
+							amet, consectetur
+							adipiscing elit. Aliquam
+							vitae convallis ex. Sed
+							ut nulla id magna
+							consectetur tincidunt.
+							Quisque rhoncus ipsum ut
+							iaculis iaculis. Nunc
+							non vestibulum justo, in
+							hendrerit tellus.
+							Vestibulum ac vulputate
+							nulla. Duis id eros
+							orci. Morbi sem sapien,
+							varius sit amet lorem
+							tincidunt, pulvinar
+							viverra ex. Aliquam
+							feugiat odio sem, non
+							semper augue malesuada
+							cras.
+						</p>
+						<p>
+							Lorem ipsum dolor sit
+							amet, consectetur
+							adipiscing elit. Aliquam
+							vitae convallis ex. Sed
+							ut nulla id magna
+							consectetur tincidunt.
+						</p>
+					</div>
+				</div>
 			</div>
 			<div className="instagram-section">
 				<p>Retrouve-nous sur Instagram !</p>
@@ -47,7 +68,7 @@ const About = () => {
 					/>
 				</a>
 			</div>
-		</div>
+		</>
 	);
 };
 
